b821d4c7176b19a532a46091ab9510bd70ec5b3e
sumittt3 <sumitthakur16000@gmail.com>
2025-03-18 18:59:17 +0530

    third commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { Spotlight } from "./ui/Spotlight";
-import { Button } from "./ui/moving-border";
+import { MovingBorder as Button } from "./ui/moving-border";
 function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
